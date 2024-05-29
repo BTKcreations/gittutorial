f4d18a240155fe5dd982cb5df5c637a8b6cb38ce
BTKgamer <71685920+BTKcreations@users.noreply.github.com>
2024-05-29 12:09:30 +0530

    added new folder counter

--- counter/script.js
+++ counter/script.js
@@ -27,3 +27,14 @@ function increase(){
         document.getElementById("number").style.color = "green";
     }
 }
+
+/*
+
+Description: This code does the following:
+1. It declares a function called `decrease()` that subtracts 1 from the number displayed on the page and changes the color of the number to red if it becomes negative.
+2. It declares a function called `reset()` that sets the number displayed on the page to 0 and changes the color of the number to black.
+3. It declares a function called `increase()` that adds 1 to the number displayed on the page and changes the color of the number to green if it becomes positive.
+
+*/
+
+// This is a comment to indicate that the code works as intended.
